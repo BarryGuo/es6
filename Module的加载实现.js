64820c443234f135581be909dbdb31b6c607f253
bocai <512517786@qq.com>
2017-03-22 00:25:49 +0800

    模块的浏览器加载

--- Module的加载实现.js
+++ Module的加载实现.js
@@ -4,12 +4,40 @@
 
 //1、浏览器加载
 
+//传统方法,通过<script>标签加载JavaScript脚本。
+//加载方式有同步加载和异步加载
+
+//同步加载
+//加载页面内嵌的脚本
+// <script type = "application/javascript">
+//         //module code
+// </script>
+//加载外部脚本
+// <script type = "application/javascript" src="path/to/myModule.js">
+// </script>
+
+//异步加载。改善用户体验
+//渲染完再执行,顺序加载
+// <script src = "path/to/myModule.js" defer></script>
+//下载完就执行,不保证顺序加载
+// <script src = "path/to/myModule.js" async></script>
+
+
+//ES6模块,也使用<script>标签,但是要加入type="module"属性
+// <script type="module" src="foo.js"></script>  //相当于传统方法的defer加载方式
+//也可以打开async属性。
+
+
+
 
 
 
 
 
 //2、ES6 模块与 CommonJS 模块的差异
+//它们有两个重大差异:
+//CommonJS值输出的是一个值得拷贝,ES6模块输出的是值得引用
+//CommonJS模块是运行时加载,ES6模块是编译时输出接口
 
 
 
